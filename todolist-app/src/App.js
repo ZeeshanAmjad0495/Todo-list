71a2e37b0b56f88a371f71d87d050189a9e83982
ZeeshanAmjad0495 <zeeshan04956@gmail.com>
2020-10-07 19:49:57 +0500

    First attempt

---
+++ todolist-app/src/App.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import Todo from "./components/Todo";
+import { nanoid } from "nanoid";
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+function App(props) {
+  
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState(props.name);
+
+  function toggleTaskCompletion(id){
+const updatedTasks = tasks.map(task => {
+  if(id===task.id)
+  {
+    return {...task, completed: !task.completed}
+  }
+  return task;
+  });
+  setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+  function editTask(id,newName){
+    const editedTaskList = tasks.map(task => {
+      if(id === task.id){
+        return{...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  const taskList = tasks.map( task => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompletion={toggleTaskCompletion}
+      deleteTask={deleteTask}
+      editTask={editTask}
+
+    />
+  ));
+  
+  const filterList = FILTER_NAMES.map(name =>(
+    <FilterButton
+     key={name}
+     name={name}
+     isPressed={name===filter}
+     setFilter={setFilter}
+     
+     />
+  ));
+  function addTask(name) {
+    const newTask = {id:"id", name: name, completed: false };
+    setTasks([...tasks,newTask]);
+  }
+ 
+  const taskNoun = taskList.length !==1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${taskNoun} remaining`;
+
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+       
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
